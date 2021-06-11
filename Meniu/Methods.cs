3b30ca07bf1db9830a5d81e499a5e7058366bba6
Alex Popescu <ion-alexandru.popescu@continental.com>
2021-06-11 12:29:04 +0300

    Update Methods.cs
    Adaugare metode GetMaxPrice () ,GetMaxWaittingTime()

--- Meniu/Methods.cs
+++ Meniu/Methods.cs
@@ -2,10 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Meniu.Models;
 
 namespace Meniu
 {
     public class Methods
     {
+        public float GetMaxPrice(List<Food> input)
+        {
+            float max = 0;
+            foreach (var items in input)
+                foreach (var item in input)
+            {
+                if (item.price > max)
+                    max = item.price;
+            }
+            return max;
+
+        }
+        public int GetMaxWaittingTime(List<Food> input)
+        {
+            int max = 0;
+            foreach (var items in input)
+                foreach (var item in input)
+                {
+                    if (item.preparationTime > max)
+                        max = item.preparationTime;
+                }
+            return max;
+
+        }
+
     }
 }
